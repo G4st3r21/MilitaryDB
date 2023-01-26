49decfc459e43c0adedb06518a87fea2fa840acc
G4st3r21 <aporyadin003@gmail.com>
2023-01-26 15:36:28 +0300

    Initial commit

---
+++ MilitaryMuseum/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Combatants)
+admin.site.register(Pictures)
---
+++ MilitaryMuseum/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class MilitaryMuseumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'MilitaryMuseum'
+    verbose_name = 'Военный музей'
